b64afae2a1cfcad359346846680723ccd5db2994
hazemelhadad <66755478+hazemelhadad@users.noreply.github.com>
2024-05-02 15:22:55 +0300

    profileEdit_Persentage

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post("/register", async (req, res) => {
     const { name,email, password } = req.body;
     const newUser = new User({ name,email, password });
     await newUser.save();
-    res.status(201).json({ message: "User registered successfully"});
+    res.status(201).json({ message: "User registered successfully",userId:newUser.id});
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -50,7 +50,7 @@ app.post("/login", async (req, res) => {
     const token = jwt.sign({ id: user._id, name: user.name }, "secret_key", {
       expiresIn: "1h",
     });
-    res.json({ token, name: user.name });
+    res.json({ token, name: user.name , userId: user._id});
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -77,14 +77,49 @@ app.post("/submit-form", (req, res) => {
 
   // Calculate percentage of 'yes' answers
   const percentageYes = (positiveResponses / totalQuestions) * 100;
+    res.status(200).send({ percentage: percentageYes });
 
   // Determine if the percentage is 70% or more
-  if (percentageYes >= 70) {
-    res.status(200).send({ message: "He is injured", injuryStatus: true });
-  } else {
-    res.status(200).send({ message: "He isn't injured", injuryStatus: false });
+  // if (percentageYes >= 70) {
+  //   res.status(200).send({ message: "He is injured", injuryStatus: true });
+  // } else {
+  //   res.status(200).send({ message: "He isn't injured", injuryStatus: false });
+  // }
+});
+/////////////////////////////////////////
+// Profile View Endpoint
+app.get("/profile/:userId", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
+
+// Profile Update Endpoint
+app.put("/profile/:userId", async (req, res) => {
+  const { name, password } = req.body;
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    user.name = name || user.name;
+    if (password) {
+      user.password = await bcrypt.hash(password, 10);
+    }
+    await user.save();
+    res.status(200).json({message:"Profile updated successfully"});
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
 
 
 
